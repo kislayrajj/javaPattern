8d5c643e5707280861c0fa0adfbc1e7b1539309b
kislayrajj <kislayraj751@gmail.com>
2024-07-25 22:26:56 +0530

    exemplary pattern commin

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import patterns.Pattern1;
+
 public class Main {
+    //patter 1
+
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Pattern1.printPattern();
     }
 }
---
+++ src/patterns/Pattern1.java
@@ -0,0 +1,13 @@
+package patterns;
+
+// exemplary pattern
+public class Pattern1 {
+    public static void printPattern(){
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
